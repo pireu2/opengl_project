dfa22e4b5dba70bb1496a8ae4f47a6b03c9d50a7
pireu2 <sebi.duica@gmail.com>
2025-01-13 19:58:16 +0200

    added trees

---
+++ src/Tree.cpp
@@ -0,0 +1,83 @@
+#include <Tree.hpp>
+
+namespace gps
+{
+    void Tree::init()
+    {
+        heightMapTexture = Model3D::ReadTextureFromFile(RESOURCES_PATH "textures/heightmap.png");
+        treeTexture = Model3D::ReadTextureFromFile(RESOURCES_PATH "objects/palm/palm.png");
+    }
+
+    void Tree::initUniforms(const glm::mat4 &model, const glm::mat4 &view, const glm::mat4 &projection, const glm::mat3 &normalMatrix, const glm::vec3 &lightDir, const glm::vec3 &lightColor)
+    {
+        shader.useShaderProgram();
+        shader.setMat4("model", glm::value_ptr(model));
+        shader.setMat4("view", glm::value_ptr(view));
+        shader.setMat4("projection", glm::value_ptr(projection));
+        shader.setMat3("normalMatrix", glm::value_ptr(normalMatrix));
+        shader.setVec3("lightDir", glm::value_ptr(lightDir));
+        shader.setVec3("lightColor", glm::value_ptr(lightColor));
+    }
+
+    void Tree::drawImguiControls()
+    {
+        ImGui::Begin("Tree Controls");
+        ImGui::SliderFloat("Wind Strength", &windStrength, 0.0f, 1.0f);
+        ImGui::End();
+    }
+
+    void Tree::render(const glm::mat4 &view, const glm::mat4 &projection, const glm::mat3 &normalMatrix, const glm::vec3 &lightDir)
+    {
+        glDisable(GL_CULL_FACE);
+        shader.useShaderProgram();
+        shader.setMat4("view", glm::value_ptr(view));
+        shader.setMat4("projection", glm::value_ptr(projection));
+        shader.setMat3("normalMatrix", glm::value_ptr(normalMatrix));
+        shader.setVec3("lightDir",glm::value_ptr(lightDir));
+        shader.setFloat("heightScale", heightScale);
+        shader.setFloat("time", static_cast<float>(glfwGetTime()));
+        shader.setFloat("windStrength", windStrength);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, heightMapTexture);
+        shader.setInt("heightMap", 1);
+        glActiveTexture(GL_TEXTURE2);
+        glBindTexture(GL_TEXTURE_2D, treeTexture);
+        shader.setInt("treeTexture", 2);
+
+        glBindVertexArray(model.getVAO());
+        glEnableVertexAttribArray(3);
+        glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+        glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), nullptr);
+        glVertexAttribDivisor(3, 1);
+        glDrawArraysInstanced(GL_TRIANGLES, 0, model.getVertexCount(), instancePositions.size());
+        glBindVertexArray(0);
+
+        glEnable(GL_CULL_FACE);
+    }
+
+    std::vector<glm::vec3> Tree::generateTreePositions()
+    {
+        std::vector<glm::vec3> positions;
+        std::default_random_engine generator;
+        std::uniform_real_distribution<float> distribution(-spacing / 2.0f, spacing / 2.0f);
+
+        for (int x = -gridSize / 2; x < gridSize / 2; ++x)
+        {
+            for (int z = -gridSize / 2; z < gridSize / 2; ++z)
+            {
+                float offsetX = distribution(generator);
+                float offsetZ = distribution(generator);
+                glm::vec3 position(x * spacing + offsetX, 0.0f, z * spacing + offsetZ);
+
+                // Check if the position is within the circle
+                if (glm::length(glm::vec2(position.x, position.z)) <= radius)
+                {
+                    positions.emplace_back(position);
+                }
+            }
+        }
+        return positions;
+    }
+
+
+}
